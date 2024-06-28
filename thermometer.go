4785e927778032f8ee01806bb1c21a83ba52639c
Eric Billingsley <ebillingsley@cloudbees.com>
2024-06-28 12:19:03 -0700

    log issue around getDischargeTime

--- thermometer.go
+++ thermometer.go
@@ -150,7 +150,7 @@ func (t *GpioThermometer) getDischargeTime() time.Duration {
 	// Set to input
 	t.pin.InputEdge(pull, edge)
 	time.Sleep(80 * time.Microsecond)
-	dt, state := t.pin.WaitForEdge(time.Second)
+	dt, state := t.pin.WaitForEdge(-1)
 	if !state {
 		Info("TIMED_OUT Thermometer %s (%s, %s) %s max(%s): %s %0.1fF", t.name, pull, edge, dt, maxTime, t.pin.Read(), toFarenheit(t.getTemp(t.getOhms(dt))))
 		return time.Duration(0)
