a711464fa1fedd743a3aa1f51c1bc64e6e8395aa
Eric Billingsley <ebillingsley@cloudbees.com>
2024-06-30 12:11:50 -0700

    work on edge detection

--- gpio_linux_arm.go
+++ gpio_linux_arm.go
@@ -140,17 +140,17 @@ func (g *Gpio) Watch(h NotificationHandler, p Pull, e Edge, s GpioState) error {
 				scnt.state = val
 				scnt.count = 1
 				scnt.time = time.Now()
-			}
-			n := Notification{
-				Pin:   g.Pin(),
-				Time:  time.Now(),
-				Value: val,
-			}
-			Info("Sending Notification[%d]: %s", g.gpioPin, n)
-			err := h(n)
-			if err != nil {
-				Info("Handler Error: watcher exited after %s: pin(%d) d(%d/%d) %v", time.Since(start), g.gpioPin, detections.lows, detections.highs, err)
-				break
+				n := Notification{
+					Pin:   g.Pin(),
+					Time:  time.Now(),
+					Value: val,
+				}
+				Info("Sending Notification[%d]: %s", g.gpioPin, n)
+				err := h(n)
+				if err != nil {
+					Info("Handler Error: watcher exited after %s: pin(%d) d(%d/%d) %v", time.Since(start), g.gpioPin, detections.lows, detections.highs, err)
+					break
+				}
 			}
 		}
 		g.gpioPin.Detect(rpio.NoEdge)
