4d9c89383e5780ffc26abad67ca8a07b5ec27dd2
Eric Billingsley <ebillingsley@cloudbees.com>
2024-06-30 11:07:35 -0700

    work on edge detection

--- gpio_linux_arm.go
+++ gpio_linux_arm.go
@@ -92,6 +92,9 @@ func (g *Gpio) Watch(h NotificationHandler, e Edge, s GpioState) error {
 		nodetections := 0
 		lows := 0
 		highs := 0
+		g.gpioPin.Output()
+		g.gpioPin.Write(rpio.Low)
+		g.gpioPin.Input()
 		g.gpioPin.Detect(rEdge(e))
 		for i := 0; i < 1000; i++ {
 			val := Low
