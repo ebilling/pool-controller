aba464418fdc2443cfcd7f02162d3f7f791ecfb6
Eric Billingsley <ebillingsley@cloudbees.com>
2024-06-30 14:36:48 -0700

    work on edge detection

--- thermometer.go
+++ thermometer.go
@@ -141,7 +141,7 @@ func (t *GpioThermometer) Accessory() *accessory.Accessory {
 }
 
 func (t *GpioThermometer) startWatcher() {
-	t.pin.Watch(t.handler, Float, FallingEdge, Low)
+	t.pin.Watch(t.handler, Float, RisingEdge, High)
 }
 
 func (t *GpioThermometer) handler(n Notification) error {
